f0b67c2e0162ae53ee8ff9aadb875a169f193d21
David Umanah <david_umanah@yahoo.com>
2023-10-29 12:23:40 +0100

    style updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import Layout from "../components/layout";
 import { StatsGrid } from "../components/StatsGrid";
+import { DashboardTable } from "../components/DashboardTable";
+import { Paper, Group, Text, Button } from "@mantine/core";
+import { DateInput } from "@mantine/dates";
 
 export default function Home() {
   return (
@@ -11,6 +14,18 @@ export default function Home() {
       <Layout>
         <section className={""}>
           <StatsGrid />
+          <Paper withBorder p={"md"} radius={"md"}>
+            <Group justify="space-between" align="center" className="py-8">
+              <Text>Transaction History</Text>
+              <Group>
+                <DateInput placeholder="Select dates" />
+                <Button className="border-1 border-gray-400 text-gray-400">
+                  View report
+                </Button>
+              </Group>
+            </Group>
+            <DashboardTable />
+          </Paper>
         </section>
       </Layout>
     </>
--- pages/streams/index.tsx
+++ pages/streams/index.tsx
@@ -40,46 +40,57 @@ export default function Streams() {
   ];
   return (
     <Layout>
-      <Stack>
-        <Paper withBorder radius={"md"} p={"md"}>
-          <Stack>
-            <Text>Income Stream</Text>
-            <Paper className="w-60" withBorder p="md" radius="md">
-              <Group justify="space-between">
-                <Text size="xs" c="dimmed">
-                  Total Balance
-                </Text>
-                <IconDotsVertical />
-              </Group>
-              <Group className="pt-8">
-                <Text>100,635.15</Text>
-                <IconEye />
-                <Badge leftSection={<IconArrowUp size={12} />}>10%</Badge>
+      <section className="bg-purple-100 p-6">
+        <Stack>
+          <Paper withBorder radius={"md"} p={"md"}>
+            <Stack>
+              <Text>Income Stream</Text>
+              <Paper className="w-60" withBorder p="md" radius="md">
+                <Group justify="space-between">
+                  <Text size="xs" c="dimmed">
+                    Total Balance
+                  </Text>
+                  <IconDotsVertical />
+                </Group>
+                <Group className="pt-8">
+                  <Text>100,635.15</Text>
+                  <IconEye />
+                  <Badge leftSection={<IconArrowUp size={12} />}>10%</Badge>
+                </Group>
+              </Paper>
+            </Stack>
+          </Paper>
+          <Text>Income Streams</Text>
+          <Flex gap={"md"}>
+            {streams.map((stream, index) => {
+              return (
+                <Link key={index} href="/streams/23432">
+                  <StreamCard detail={stream} />
+                </Link>
+              );
+            })}
+          </Flex>
+          <Group justify="end">
+            <Anchor className="text-black" href="/">
+              See more
+            </Anchor>
+            <IconArrowRight />
+          </Group>
+          <Paper
+            withBorder
+            p="md"
+            radius="md"
+            className="bg-transparent border-black"
+          >
+            <Paper p="md" radius="md" className="bg-purple-200">
+              <Group justify="start">
+                <Text>Received payments</Text>
               </Group>
             </Paper>
-            <Text>Income Streams</Text>
-            <Flex gap={"md"}>
-              {streams.map((stream, index) => {
-                return (
-                  <Link key={index} href="/streams/23432">
-                    <StreamCard detail={stream} />
-                  </Link>
-                );
-              })}
-            </Flex>
-          </Stack>
-        </Paper>
-        <Group justify="end">
-          <Anchor href="/">See more</Anchor>
-          <IconArrowRight />
-        </Group>
-        <Paper p="md" radius="md" className="bg-purple-200">
-          <Group justify="start">
-            <Text>Received payments</Text>
-          </Group>
-        </Paper>
-        <StreamsTable />
-      </Stack>
+            <StreamsTable />
+          </Paper>
+        </Stack>
+      </section>
     </Layout>
   );
 }
